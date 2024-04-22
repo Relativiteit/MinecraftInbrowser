a35613cf2c2a8fca321839c6f2d2dcd5eed2e0ae
Relativiteit <alejocain@gmail.com>
2024-04-22 17:02:34 +0200

    added images

---
+++ src/images/textures.js
@@ -0,0 +1,16 @@
+import { TextureLoader } from "three"
+
+import {
+dirtImg,
+grassImg,
+glassImg,
+woodImg,
+logImg,
+} from "./images.js"
+
+const dirtTexture = new TextureLoader(dirtImg)
+const dirtTexture = new TextureLoader(grassImg)
+const dirtTexture = new TextureLoader(dirtImg)
+const dirtTexture = new TextureLoader(dirtImg)
+const dirtTexture = new TextureLoader(dirtImg)
+const dirtTexture = new TextureLoader(dirtImg)
