42ad39a0cec3abbe93e48d944e69709f54347df2
Relativiteit <alejocain@gmail.com>
2024-04-22 22:27:53 +0200

    jumping implemented

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,8 +2,15 @@ import { useSphere } from "@react-three/cannon";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard";
+
+
+const JUMP_FORCE = 4
 
 export const PLayer = () => {
+    const actions = useKeyboard()
+    console.log('actions', Object.entries(actions).filter(([k, v]) => v))
+
     const { camera } = useThree()
     const [ref, api] = useSphere(() => ({
         mass: 1,
@@ -24,7 +31,11 @@ export const PLayer = () => {
     useFrame(() => {
         console.log('frame')
         camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
-        api.velocity.set(0,0,0)
+
+        if(actions.jump) {
+        api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2])
+
+        }
     })
     return (
         <mesh ref={ref}></mesh>
